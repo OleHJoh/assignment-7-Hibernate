bfa7c96a7e2729593d4698db19b5ed47b5c4ec5f
Ole Henrik Johansen <johansen.ole.h@gmail.com>
2022-03-04 17:55:59 +0100

    Initial commit

---
+++ src/main/java/noroff/accelerate/assignment7Hibernate/Assignment7HibernateApplication.java
@@ -0,0 +1,13 @@
+package noroff.accelerate.assignment7Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment7HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment7HibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noroff/accelerate/assignment7Hibernate/Assignment7HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package noroff.accelerate.assignment7Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment7HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
